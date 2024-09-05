7a407c231fb7c63c43c034cece6ad4312bdf2992
gmostafa1210 <gmostafa1210@gmail.com>
2024-09-05 13:14:31 +0600

    [ADD] basic module structure

---
+++ gm_portal/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+##############################################################
+#                                                            #
+#   G. Mostafa                                               #
+#   Copyright (C) 2024 (https://gmostafa1210.github.io/)     #
+#                                                            #
+##############################################################
+
+{
+    'name': 'Portal Practice',
+    'version': '15.0.1.0',
+    'sequence': 1,
+    'category': 'Portal',
+    'summary': 'Portal Practice Summary',
+    'description': """Portal Practice Description.""",
+    'author': 'Golam Mostafa',
+    'website': 'https://gmostafa1210.github.io/',
+    'depends': [
+        'base', 'portal',
+    ],
+    'data': [
+        # 'views/portal_template_views.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
