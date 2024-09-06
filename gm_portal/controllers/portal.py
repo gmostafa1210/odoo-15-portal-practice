9df8c5b48d0b480df2d57dd46e6b1499c52dc415
gmostafa1210 <gmostafa1210@gmail.com>
2024-09-06 15:58:01 +0600

    [ADD] list view in controller

--- gm_portal/controllers/portal.py
+++ gm_portal/controllers/portal.py
@@ -7,6 +7,7 @@
 ############################################################
 
 from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo import http
 from odoo.http import request
 
 
@@ -16,3 +17,10 @@ class GmCustomerPortal(CustomerPortal):
         res = super()._prepare_home_portal_values(counters)
         res['student_count'] = request.env['gm.student'].sudo().search_count([])
         return res
+    
+    @http.route(['/my/students'], type='http', website=True)
+    def gm_student_list_view(self, **ka):
+        STUDENT = request.env['gm.student']
+        students = STUDENT.search([])
+        vals = {'students': students, 'page_name': 'student_list'} # contreller lavel que context
+        return request.render("gm_portal.gm_student_list_view_portal", vals) 
